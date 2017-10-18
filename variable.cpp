58ad88499b89b8def75c581a029331a11cf41394
賴偉程 <arnold2841@gmail.com>
2017-10-19 02:39:22 +0800

    hw3

--- variable.cpp
+++ variable.cpp
@@ -87,22 +87,13 @@ bool Variable::match(Atom& atom){
 
 bool Variable::match(Variable& x){
   bool result=_assignable;
-  _memory.push_back(&x._value);
-  x._memory.push_back(&_value);
-  if(&x._value!=&_value){
-    for(int i=0;i<_memory.size();i++){
-      x._memory.push_back(_memory[i]);
-    }
-    for(int i=0;i<x._memory.size();i++){
-      _memory.push_back(x._memory[i]);
-    }
-  }
+
   if(_assignable&&x._assignable){
     /*string *tmp=x._value;
     cout<<tmp<<endl;*/
     result=true;
 
-    /*_memory.push_back(&x._value);
+    _memory.push_back(&x._value);
     x._memory.push_back(&_value);
     if(&x._value!=&_value){
       for(int i=0;i<_memory.size();i++){
@@ -111,7 +102,7 @@ bool Variable::match(Variable& x){
       for(int i=0;i<x._memory.size();i++){
         _memory.push_back(x._memory[i]);
       }
-    }*/
+    }
 
     x._value=_value;
     matched=true;
@@ -124,12 +115,14 @@ bool Variable::match(Variable& x){
     result=true;
 
     notassign();
+    matched=true;
     change(x._value);
 
   }else if(!_assignable&&x._assignable){
     result=true;
 
     x.notassign();
+    x.matched=true;
     for(int i=0 ; i<x._memory.size() ; i++){
       *x._memory[i] = _value;
     }
